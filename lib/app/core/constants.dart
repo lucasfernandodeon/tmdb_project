73a087b72c6866d11e51a15fdeac86f12ae5e774
lucasfernandodeon <lucasfernandodeon@gmail.com>
2023-01-21 15:39:47 -0300

    adicionado endpoints

---
+++ lib/app/core/constants.dart
@@ -0,0 +1,20 @@
+
+
+class Constants {
+  static const _baseEndpoint = "https://api.themoviedb.org/3/movie";
+  static const String _posterEnpoint = "https://image.tmdb.org/t/p/original";
+  static const String _nowPlayingEndpoint = "$_baseEndpoint/now_playing";
+  static const String _key = "CHAVE";
+
+  static Uri returnNowPlayingUri({required int page}){
+    return Uri.parse("$_nowPlayingEndpoint?api_key=$_key&page=$page");
+  }
+
+  static Uri returnMovieDetailsUri({required int movieId}){
+    return Uri.parse("$_baseEndpoint/$movieId?api_key=$_key");
+  }
+
+  static String returnPosterUrl({required String posterPath}){
+    return "$_posterEnpoint$posterPath";
+  }
+}
--- lib/app/repositories/now_playing_api_repository.dart
+++ lib/app/repositories/now_playing_api_repository.dart
@@ -1,10 +1,21 @@
+import 'dart:convert';
+
 import 'package:tmdb_project/app/models/now_playing_response_model.dart';
 import 'package:tmdb_project/app/repositories/now_playing_repository.dart';
+import 'package:http/http.dart'as http;
+
+import '../core/constants.dart';
 
 class NowPlayingApiRepository implements NowPlayingRepository {
+
+  final http.Client httpClient;
+
+  NowPlayingApiRepository({required this.httpClient});
+  
+
   @override
-  Future<NowPlayingResponseModel> getNowPlaying({required int page}) {
-    // TODO: implement getNowPlaying
-    throw UnimplementedError();
+  Future<NowPlayingResponseModel> getNowPlaying({required int page}) async{
+    var response = await httpClient.get(Constants.returnNowPlayingUri(page: page));
+    return  NowPlayingResponseModel.fromJson(jsonDecode(response));
   }
 }
